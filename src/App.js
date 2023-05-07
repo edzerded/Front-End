8a5d20f9f6abc94e2a146133ccc377b319b0ca0c
edzerded <90583800+edzerded@users.noreply.github.com>
2023-05-07 19:35:06 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi aplicación React</h1>
+      <p>Bienvenido a mi aplicación.</p>
+    </div>
+  );
+}
+
+export default App;
